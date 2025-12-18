74bda8bcd827e4a1daf2023be4fcaed9743a577e
bala-amypo <bala@amypo.in>
2025-12-18 06:50:47 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import java.util.List;
 
 import com.example.demo.model.UserEntity;
 import com.example.demo.service.UserService;
@@ -17,25 +16,5 @@ public class UserController {
     @PostMapping("/adduser")
     public UserEntity createUser(@RequestBody UserEntity user) {
         return userService.createUser(user);
-    }
-
-    @GetMapping("/showusers")
-    public List<UserEntity> showUsers() {
-        return userService.getAllUsers();
-    }
-
-    @GetMapping("/user/{id}")
-    public UserEntity getUserById(@PathVariable Long id) {
-        return userService.getUserById(id).orElseThrow(() -> new RuntimeException("User not found"));
-    }
-
-    @PutMapping("/user/{id}")
-    public UserEntity updateUser(@PathVariable Long id, @RequestBody UserEntity user) {
-        return userService.updateUser(id, user);
-    }
-
-    @DeleteMapping("/user/{id}")
-    public void deleteUser(@PathVariable Long id) {
-        userService.deleteUser(id);
     }
 }
