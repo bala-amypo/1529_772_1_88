c8d7febe77fea499f5a465462b2784f6fbd40818
bala-amypo <bala@amypo.in>
2025-12-21 16:36:51 +0000

    User commit

--- src/main/java/com/example/demo/repository/ApartmentUnitRepository.java
+++ src/main/java/com/example/demo/repository/ApartmentUnitRepository.java
@@ -1,11 +1,16 @@
 package com.example.demo.repository;
+
 import com.example.demo.model.ApartmentUnit;
-import com.example.demo.model.UserEntity;
+import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
 @Repository
 public interface ApartmentUnitRepository extends JpaRepository<ApartmentUnit, Long> {
-    Optional<ApartmentUnit> findByUser(UserEntity user);
+
+    Optional<ApartmentUnit> findByOwner(User owner);
+
+    boolean existsByUnitNumber(String unitNumber);
 }
