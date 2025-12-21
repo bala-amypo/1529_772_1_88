1b274878ee820db857c0b284d45665c7d42c6b53
bala-amypo <bala@amypo.in>
2025-12-21 16:57:51 +0000

    User commit

--- src/main/java/com/example/demo/controller/ApartmentUnitController.java
+++ src/main/java/com/example/demo/controller/ApartmentUnitController.java
@@ -2,33 +2,22 @@ package com.example.demo.controller;
 
 import com.example.demo.model.ApartmentUnit;
 import com.example.demo.service.ApartmentUnitService;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/units")
 public class ApartmentUnitController {
 
+    @Autowired
     private ApartmentUnitService apartmentUnitService;
 
-    public ApartmentUnitController(ApartmentUnitService apartmentUnitService) {
-        this.apartmentUnitService = apartmentUnitService;
+    @PostMapping("/assignunit")
+    public ApartmentUnit assignUnitToUser(@RequestParam Long userId, @RequestBody ApartmentUnit unit) {
+        return apartmentUnitService.assignUnitToUser(userId, unit);
     }
 
-    @PostMapping("/assign/{userId}")
-    public ResponseEntity<ApartmentUnit> assignUnitToUser(
-            @PathVariable Long userId,
-            @RequestBody ApartmentUnit unit) {
-
-        return ResponseEntity.ok(
-                apartmentUnitService.assignUnitToUser(userId, unit)
-        );
-    }
-
-    @GetMapping("/user/{userId}")
-    public ResponseEntity<ApartmentUnit> getUnitByUser(@PathVariable Long userId) {
-        return ResponseEntity.ok(
-                apartmentUnitService.getUnitByUser(userId)
-        );
+    @GetMapping("/unit/{userId}")
+    public ApartmentUnit getUnitByUser(@PathVariable Long userId) {
+        return apartmentUnitService.getUnitByUser(userId);
     }
 }
