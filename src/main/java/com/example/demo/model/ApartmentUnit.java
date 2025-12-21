11ab743222f6bcb65487fb5fcd3e016316378a65
bala-amypo <bala@amypo.in>
2025-12-21 17:06:50 +0000

    User commit

--- src/main/java/com/example/demo/model/ApartmentUnit.java
+++ src/main/java/com/example/demo/model/ApartmentUnit.java
@@ -1,52 +1,27 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.JoinColumn;
+import jakarta.persistence.*;
 
 @Entity
 public class ApartmentUnit {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String unitNumber;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
     private UserEntity user;
 
-    public ApartmentUnit() {
-    }
+    public ApartmentUnit() {}
 
-    public ApartmentUnit(String unitNumber, UserEntity user) {
-        this.unitNumber = unitNumber;
-        this.user = user;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public Long getId() {
-        return id;
-    }
+    public String getUnitNumber() { return unitNumber; }
+    public void setUnitNumber(String unitNumber) { this.unitNumber = unitNumber; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUnitNumber() {
-        return unitNumber;
-    }
-
-    public void setUnitNumber(String unitNumber) {
-        this.unitNumber = unitNumber;
-    }
-
-    public UserEntity getUser() {
-        return user;
-    }
-
-    public void setUser(UserEntity user) {
-        this.user = user;
-    }
+    public UserEntity getUser() { return user; }
+    public void setUser(UserEntity user) { this.user = user; }
 }
