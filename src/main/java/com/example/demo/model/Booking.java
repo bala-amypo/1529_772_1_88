88b2a52dc759573b898398793d36b6871c1c5f4c
bala-amypo <bala@amypo.in>
2025-12-25 18:21:03 +0000

    User commit

--- src/main/java/com/example/demo/model/Booking.java
+++ src/main/java/com/example/demo/model/Booking.java
@@ -1,11 +1,29 @@
 package com.example.demo.model;
 
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+
+@Entity
 public class Booking {
 
+    public static final String STATUS_CONFIRMED = "CONFIRMED";
+    public static final String STATUS_CANCELLED = "CANCELLED";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long userId;
-    private Long facilityId;
+
     private String status;
+
+    private LocalDateTime bookingTime;
+
+    public Booking() {}
+
+    public Booking(Long id, Booking booking, String status, LocalDateTime bookingTime) {
+        this.id = id;
+        this.status = status;
+        this.bookingTime = bookingTime;
+    }
 
     public Long getId() {
         return id;
@@ -15,27 +33,19 @@ public class Booking {
         this.id = id;
     }
 
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public Long getFacilityId() {
-        return facilityId;
-    }
-
-    public void setFacilityId(Long facilityId) {
-        this.facilityId = facilityId;
-    }
-
     public String getStatus() {
         return status;
     }
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public LocalDateTime getBookingTime() {
+        return bookingTime;
+    }
+
+    public void setBookingTime(LocalDateTime bookingTime) {
+        this.bookingTime = bookingTime;
     }
 }
