4007b61e99037ed3de2dace36ac2ee4c98e89ed3
bala-amypo <bala@amypo.in>
2025-12-25 18:51:20 +0000

    User commit

--- src/main/java/com/example/demo/model/ApartmentUnit.java
+++ src/main/java/com/example/demo/model/ApartmentUnit.java
@@ -2,20 +2,49 @@ package com.example.demo.model;
 
 public class ApartmentUnit {
 
-    private String unitNumber;
+    private Long id;
+    private String name;
     private int floor;
-    private User user;
+    private User owner;
 
     public ApartmentUnit() {
     }
 
-    public ApartmentUnit(String unitNumber, int floor, User user) {
-        this.unitNumber = unitNumber;
+    public ApartmentUnit(String name, int floor, User owner) {
+        this.name = name;
         this.floor = floor;
-        this.user = user;
+        this.owner = owner;
     }
 
-    public String getUnitNumber() { return unitNumber; }
-    public int getFloor() { return floor; }
-    public User getUser() { return user; }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
 }
