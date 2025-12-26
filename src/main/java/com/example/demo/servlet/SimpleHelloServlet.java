a7bb7adf3d574ff71456ab5fe20682dfd0744390
bala-amypo <bala@amypo.in>
2025-12-26 09:44:06 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleHelloServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleHelloServlet.java
@@ -3,24 +3,20 @@ package com.example.demo.servlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.ServletException;
+
 import java.io.IOException;
 
 public class SimpleHelloServlet extends HttpServlet {
 
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse res)
-            throws ServletException, IOException {
-
-        res.setStatus(HttpServletResponse.SC_OK);
-        res.setContentType("text/plain");
-        res.getWriter().write("Hello from Simple Servlet");
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/plain");
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().write("Hello from Simple Servlet");
     }
 
     @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse res)
-            throws ServletException, IOException {
-
-        res.setStatus(HttpServletResponse.SC_OK);
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setStatus(HttpServletResponse.SC_OK);
     }
 }
